cd537723d47a40928048ffb6a573e0d5aba03e86
Edryn <edryn.magcalas@gmail.com>
2023-11-18 00:01:28 +0800

    list entries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,8 +87,7 @@ dependencies {
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
     // Coil
-    implementation ("io.coil-kt:coil:2.5.0")
-    implementation ("com.google.accompanist:accompanist-coil:0.15.0")
+    implementation ("io.coil-kt:coil-compose:2.5.0")
 
     // Dagger - Hilt
     implementation ("com.google.dagger:hilt-android:2.48.1")
